c180dc2bbbd90af57fd2a47210784843e31dd4a1
Archi Jain <51859790+JainArchi06@users.noreply.github.com>
2023-04-22 23:03:44 +0530

    Learn update

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
+#runtime PolyMorphism
+class parent:
+    name="Tom"
+class child(parent):
+    name="jerry"
 
+obj=child()
+print(obj.name)
+
+# using pass method
+class parent:
+    name="Tom"
+class child(parent):
+    pass
+
+obj=child()
+print(obj.name)
+
+#Compiletime Polymorphism
+class Bank:
+    def rateofinterest(self):
+        return 0
+class icici(Bank):
+    def rateofinterest(self):
+        return 10.2
+obj=icici()
+print(obj.rateofinterest())
